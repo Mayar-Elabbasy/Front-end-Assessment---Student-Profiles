f00431b6a23b619b814bc33451048eeac01120cf
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-05-08 00:17:24 +0200

    handled clear (Add a tag) input field after enter is pressed

--- src/components/all-students/index.js
+++ src/components/all-students/index.js
@@ -6,7 +6,6 @@ import { FaPlus, FaMinus } from 'react-icons/fa';
 function AllStudents() {
     const [state, setState] = useState({
         students: [],
-        searchByStudentName: ''
     });
 
     useEffect(() => {
@@ -57,13 +56,27 @@ function AllStudents() {
     }
 
     const addNewTag = (event, studentID) => { 
+        /* 
+        This function handles adding new tag as it 
+        first of all, check if enter is pressed then it loops over all the students
+        and make sure that the student's id matches the arguments passed with the function
+        and after checking the tag is added to the array of tags specified for this student 
+        and finally, clear the input filed after adding the tag successfully in the array
+        */
+
         if(event.key === 'Enter') {
             for(let i = 0; i < state.students.length; i++) {
-                // state.students[i].studentTags = [];
+                
                 if (state.students[i].id === studentID) {
                     state.students[i].studentTags = [...state.students[i].studentTags];
-                    state.students[i].studentTags.push(state.tag)
-                    document.querySelectorAll(".tag")[i].value = '';
+                    state.students[i].studentTags.push(state.tag);
+
+                    // clear (Add a tag) input field after enter is pressed
+                    if(document.querySelectorAll(".tag").length > 1) {
+                        document.querySelectorAll(".tag")[i].value = '';    
+                    } else {
+                        document.querySelectorAll(".tag")[0].value = '';
+                    }
 
                     setState({
                         ...state,
@@ -93,114 +106,114 @@ function AllStudents() {
             <input type="search" placeholder="Search by name" 
                    onChange={handleSearchByStudentName} />
 
-            {students.map(student => { 
-                return (
-                    <div key={student.id}>
-                        <div className="row">
-                            <div className="col-md-2">
-                                <img 
-                                    src={student.pic} 
-                                    alt={`${student.firstName} ${student.lastName}`}
-                                    className="float-left img-fluid student-avatar m-2 ml-3 
-                                               mt-3 rounded-circle"
-                                />
-                            </div>
-                            <div className="col-md-8 m-1">
-                                <h3 className="mt-1 text-left font-weight-bold text-uppercase">
-                                    {`${student.firstName} ${student.lastName}`}
-                                </h3>
-                                <CardBody className="text-left main-font">
-                                    <CardText>
-                                        <span className="student-data">
-                                            Email: {student.email}
-                                        </span>
-                                        <span className="student-data">
-                                            Company: {student.company}
-                                        </span> 
-                                        <span className="student-data">
-                                            Skill: {student.skill}
-                                        </span>
-                                        <span className="student-data">
-                                            Average: 
-                                            <span className="ml-1">
-                                            {(student.grades.length > 0)?
-                                                <React.Fragment>
-                                                {/* average of grades for each student */}
-                                                <span className="mb-2">
-                                                    {student.grades.reduce((sum, grade) =>
-                                                        sum + Number(grade), 0) / student.grades.length}
-                                                    {`${"%"}`}
-                                                    <br />
-                                                    <br />
-                                                </span>
-
-                                                {/* expandable list view for each student */}
-                                                    {student.grades.map((grade, index) => {         
-                                                        return (
-                                                            <React.Fragment key={index}>
-                                                                {state.openedExpandableListView[student.id] ?
-                                                                    <span className="grade">
-                                                                        <span className="mr-3">
-                                                                            {`Test ${index+1}:`}
-                                                                        </span>   
-                                                                        {`${grade}${"%"}`}
-                                                                    </span>
-                                                                : 
-                                                                <React.Fragment></React.Fragment>
-                                                                }
-                                                            </React.Fragment>
-                                                        ) 
-                                                        })    
-                                                    }
-                                                </React.Fragment>
-                                                :
-                                                <React.Fragment></React.Fragment>
-                                            }
+        {students.map(student => {
+            return (
+                <div key={student.id}>
+                    <div className="row">
+                        <div className="col-md-2">
+                            <img
+                                src={student.pic} 
+                                alt={`${student.firstName} ${student.lastName}`}
+                                className="float-left img-fluid student-avatar m-2 ml-3 
+                                            mt-3 rounded-circle"
+                            />
+                        </div>
+                        <div className="col-md-8 m-1">
+                            <h3 className="mt-1 text-left font-weight-bold text-uppercase">
+                                {`${student.firstName} ${student.lastName}`}
+                            </h3>
+                            <CardBody className="text-left main-font">
+                                <CardText>
+                                    <span className="student-data">
+                                        Email: {student.email}
+                                    </span>
+                                    <span className="student-data">
+                                        Company: {student.company}
+                                    </span> 
+                                    <span className="student-data">
+                                        Skill: {student.skill}
+                                    </span>
+                                    <span className="student-data">
+                                        Average: 
+                                        <span className="ml-1">
+                                        {(student.grades.length > 0)?
+                                            <React.Fragment>
+                                            {/* average of grades for each student */}
+                                            <span className="mb-2">
+                                                {student.grades.reduce((sum, grade) =>
+                                                    sum + Number(grade), 0) / student.grades.length}
+                                                {`${"%"}`}
+                                                <br />
+                                                <br />
                                             </span>
-                                        </span>
-                                    </CardText>
-                                    {(student.studentTags && student.studentTags.length > 0)?
-                                        <React.Fragment>
-                                            {student.studentTags.map(
-                                                (studentTag, index) => {         
+
+                                            {/* expandable list view for each student */}
+                                                {student.grades.map((grade, index) => {
                                                     return (
                                                         <React.Fragment key={index}>
-                                                            <span className="student-tag">
-                                                                {studentTag}
-                                                            </span>
+                                                            {state.openedExpandableListView[student.id] ?
+                                                                <span className="grade">
+                                                                    <span className="mr-3">
+                                                                        {`Test ${index+1}:`}
+                                                                    </span>
+                                                                    {`${grade}${"%"}`}
+                                                                </span>
+                                                            : 
+                                                            <React.Fragment></React.Fragment>
+                                                            }
                                                         </React.Fragment>
                                                     ) 
-                                                })    
-                                            }
-                                        </React.Fragment>
-                                        :
-                                        <React.Fragment></React.Fragment>
-                                    }
-                                </CardBody>
-                                <div className="col-md-2 mt-3">
-                                    <input 
-                                        type="text" name="tag" placeholder="Add a tag"
-                                        className="tag" 
-                                        onChange={handleFields} 
-                                        onKeyDown={(event) => addNewTag(event,student.id)} />
-                                </div>
+                                                    })  
+                                                }
+                                            </React.Fragment>
+                                            :
+                                            <React.Fragment></React.Fragment>
+                                        }
+                                        </span>
+                                    </span>
+                                </CardText>
+                                {(student.studentTags && student.studentTags.length > 0)?
+                                    <React.Fragment>
+                                        {student.studentTags.map(
+                                            (studentTag, index) => {
+                                                return (
+                                                    <React.Fragment key={index}>
+                                                        <span className="student-tag">
+                                                            {studentTag}
+                                                        </span>
+                                                    </React.Fragment>
+                                                ) 
+                                            })  
+                                        }
+                                    </React.Fragment>
+                                    :
+                                    <React.Fragment></React.Fragment>
+                                }
+                            </CardBody>
+                            <div className="col-md-2 mt-3">
+                                <input 
+                                    type="text" name="tag" placeholder="Add a tag"
+                                    className="tag"
+                                    onChange={handleFields}
+                                    onKeyDown={(event) => addNewTag(event,student.id)} />
                             </div>
-                            <div className="col-md-1">
-                                <button className="test-scores-toggle-button"
-                                    onClick={() => {toggleButton(student.id)}}>
-                                    {state.openedExpandableListView[student.id] ? 
-                                        <FaMinus size="1.50em" /> 
-                                        :
-                                        <FaPlus size="1.50em" />
-                                    }     
-                                </button>
-                            </div>
-                        </div> 
-                        <hr />
-                    </div> 
-                );
-            })
-            }  
+                        </div>
+                        <div className="col-md-1">
+                            <button className="test-scores-toggle-button"
+                                onClick={() => {toggleButton(student.id)}}>
+                                {state.openedExpandableListView[student.id] ?
+                                    <FaMinus size="1.50em" />
+                                    :
+                                    <FaPlus size="1.50em" />
+                                }   
+                            </button>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+            );
+        })
+        }  
         </Card>
     )
 }
